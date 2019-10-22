cf3efa189744ee60bc7e72546649f7cf337b7a39
Grant Erickson <grant@intellitect.com>
2019-10-22 12:08:23 -0700

    Single File Executable

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -7,6 +7,20 @@ namespace ConsoleDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            System.Console.WriteLine($"Environment.Version: {System.Environment.Version}");
+            // Old result: Environment.Version: 4.0.30319.42000
+            // New result: Environment.Version: 3.0.0
+
+            System.Console.WriteLine($"RuntimeInformation.FrameworkDescription: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
+            // Old result: RuntimeInformation.FrameworkDescription: .NET Core 4.6.27415.71
+            // New result: RuntimeInformation.FrameworkDescription: .NET Core 3.0.0-preview4-27615-11
+
+
+
+
+
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
